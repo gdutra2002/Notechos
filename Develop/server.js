91bd4c52c74e9703d8469436f281c2c05bfa0f10
gdutra2002 <119914021+gdutra2002@users.noreply.github.com>
2023-06-10 16:14:09 -0700

    server file created, jasonDB updated

---
+++ Develop/server.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const uuid = require('./helpers/uuid');
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', apiRoutes);
+app.use(express.static('public'));
+app.use('/', htmlRoutes);
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET request for notes
+app.get('/api/notes', (req, res) => {
+  // Send a message to the client
+  res.status(200).json(`${req.method} request received to get notes`);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get notes`);
+});
+
+// POST request to add a note
+app.post('/api/note', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, note, username } = req.body;
+
+  // If all the required properties are present
+  if ( title && note && username) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      note,
+      username,
+      note_id: uuid(),
+    };
+
+    // Obtain existing notes
+    fs.readFile('./db/notes.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new note
+        parsedNotes.push(newNote);
+
+        // Write updated notes back to the file
+        fs.writeFile(
+          './db/notes.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
